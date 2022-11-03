0e6b444f1252b20c2b8dbeebdebce4146d23a399
Alec Mestroni <alec.mestroni@finconsgroup.local>
2022-11-03 09:52:27 +0100

    cypress wecamb bug

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
 	e2e: {
 		baseUrl: 'https://iqp.eu-west-1.tsaws.infocert.it/',
+		// baseUrl: 'https://test',
+		// baseUrl: 'https://learn.cypress.io/',
 		setupNodeEvents(on, config) {
 			// implement node event listeners here
 		},
